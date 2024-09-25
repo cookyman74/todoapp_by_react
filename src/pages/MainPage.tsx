38ccc1b500a83a8b307bffb1faad70a381f933ac
Justin Jang <cookyman@gmail.com>
2024-09-26 01:14:08 +0900

    할일리스트를 카드섹션으로 변경

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,29 @@
 // src/pages/main/MainPage.tsx
-import React, { useEffect } from 'react';
-import { Button, Container, Typography, List, ListItem, ListItemText } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import { Button, Container, Typography, CardActions, Card, CardContent, Grid, IconButton, Collapse, Divider } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import useTodoStore from "../entities/todo/model/todoStore";
 import useUserStore from "../entities/user/model/userStore";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 
 const MainPage: React.FC = () => {
     const navigate = useNavigate();
     const { todos, addTodo, toggleTodo, removeTodo } = useTodoStore();
     const { user, clearUser } = useUserStore();
+
+    //로컬스토리지에서 완료된 할일리스트에 대한 접힘 상태를 불러옴
+    const [collapsed, setCollapse] = useState(() => {
+        const savedState = localStorage.getItem('completedTodosCollapsed');
+        return savedState ? JSON.stringify(savedState) : true;
+    });
+
+    // 접힘 상태를 localstorage에 저장
+    const handleToggleCollapse = () => {
+        const newCollapsed = !collapsed;
+        setCollapse(newCollapsed);
+        localStorage.setItem('completedTodoCollapsed', JSON.stringify(newCollapsed));
+    };
 
     useEffect(() => {
         if (!user) {
@@ -83,72 +98,145 @@ const MainPage: React.FC = () => {
 
             {user && (
                 <>
-                    <Typography variant="h5" gutterBottom>
-                        할일 통계
-                    </Typography>
-                    <Typography variant="body1" gutterBottom>
-                        {/*완료된 할일: {completedTodos}, 진행 중인 할일: {inProgressTodos}*/}
-                    </Typography>
+                <Grid container spacing={3} style={{marginTop: '20px'}}>
+                    {todos.filter(todo => !todo.completed).map((todo) => (
+                        <Grid item xs={12} sm={6} md={4} key={todo.id}>
+                            <Card>
+                                <CardContent style={{
+                                    textAlign: 'left',
+                                    textDecoration: todo.completed ? 'line-through' : 'none'
+                                }}>
+                                    <Typography variant="h6" gutterBottom style={{textAlign: 'center'}}>
+                                        {todo.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        설명: {todo.description}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        생성일: {todo.createdAt ? formatDate(todo.createdAt) : '알 수 없음'}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        기한: {todo.deadline ? formatDate(todo.deadline) : '설정되지 않음'}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        담당자: {todo.assignedTo ? todo.assignedTo : '할당되지 않음'}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        태그: {todo.tags?.join(', ') || '태그 없음'}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions style={{justifyContent: 'center'}}>
+                                    <Button
+                                        size="small"
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={() => handleStatusTodo(todo.id, !todo.completed)}
+                                    >
+                                        {todo.completed ? '미완료' : '완료'}
+                                    </Button>
+                                    <Button
+                                        size="small"
+                                        variant="outlined"
+                                        color="secondary"
+                                        onClick={() => removeTodo(todo.id)}
+                                    >
+                                        삭제
+                                    </Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
 
-                    {/* 할일 등록 버튼 */}
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleAddTodo}
-                        style={{ marginTop: '20px' }}
-                    >
-                        할일 등록
-                    </Button>
+                <div style={{marginTop: '40px'}}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                        <Typography>
+                            Completed or Deadline Todo List
+                        </Typography>
+                        <IconButton onClick={handleToggleCollapse}>
+                            {collapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider style={{ marginTop: '10px' }} />
 
-                    {/* 할일 리스트 */}
-                    <List style={{ marginTop: '20px', textAlign: 'left' }}>
-                        {todos.map((todo) => (
-                            <ListItem key={todo.id}>
-                                <ListItemText
-                                    primary={todo.title}
-                                    secondary={
-                                        <>
-                                            <div>설명: {todo.description}</div>
-                                            <div>생성일: {todo.deadline ? formatDate(todo.createdAt) : ''}</div>  {/* 생성일 표시 */}
-                                            <div>기한: {todo.deadline ? formatDate(todo.deadline) : '설정되지 않음'}</div>  {/* 기한 표시 */}
-                                            <div>담당자: {todo.assignedTo ? todo.assignedTo : '할당되지 않음'}</div>  {/* 담당자 표시 */}
-                                            <div>태그: {todo.tags?.join(', ') || '태그 없음'}</div>  {/* 태그 표시 */}
-                                        </>
-                                    }
-                                    style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
-                                />
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={() => handleStatusTodo(todo.id, !todo.completed)}
-                                    style={{ marginRight: '10px' }}
-                                >
-                                    {todo.completed ? '미완료' : '완료'}
-                                </Button>
-                                <Button
-                                    variant="outlined"
-                                    color="secondary"
-                                    onClick={() => removeTodo(todo.id)}
-                                >
-                                    삭제
-                                </Button>
-                            </ListItem>
-                        ))}
-                    </List>
+                    <Collapse in={!collapsed}>
+                        <Grid container spacing={3}>
+                            {todos.filter(todo => todo.completed).map((todo) => (
+                                <Grid item xs={12} sm={6} md={4} key={todo.id}>
+                                    <Card>
+                                        <CardContent style={{
+                                            textAlign: 'left',
+                                            textDecoration: todo.completed ? 'line-through' : 'none'
+                                        }}>
+                                            <Typography variant="h6" gutterBottom style={{textAlign: 'center'}}>
+                                                {todo.title}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary">
+                                                설명: {todo.description}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary">
+                                                생성일: {todo.createdAt ? formatDate(todo.createdAt) : '알 수 없음'}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary">
+                                                기한: {todo.deadline ? formatDate(todo.deadline) : '설정되지 않음'}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary">
+                                                담당자: {todo.assignedTo ? todo.assignedTo : '할당되지 않음'}
+                                            </Typography>
+                                            <Typography variant="body2" color="textSecondary">
+                                                태그: {todo.tags?.join(', ') || '태그 없음'}
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions style={{justifyContent: 'center'}}>
+                                            <Button
+                                                size="small"
+                                                variant="contained"
+                                                color="primary"
+                                                onClick={() => handleStatusTodo(todo.id, !todo.completed)}
+                                            >
+                                                {todo.completed ? '미완료' : '완료'}
+                                            </Button>
+                                            <Button
+                                                size="small"
+                                                variant="outlined"
+                                                color="secondary"
+                                                onClick={() => removeTodo(todo.id)}
+                                            >
+                                                삭제
+                                            </Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Collapse>
 
-                    {/* 로그아웃 버튼 */}
-                    <Button
-                        variant="outlined"
-                        color="secondary"
-                        onClick={handleLogout}
-                        style={{ marginTop: '20px' }}
-                    >
-                        로그아웃
-                    </Button>
+                    {/* 할일 등록 버튼과 로그아웃 버튼을 나란히 배치 */}
+                    <Grid container spacing={2} justifyContent="center" style={{marginTop: '20px'}}>
+                        <Grid item>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={handleAddTodo}
+                            >
+                                할일 등록
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                variant="outlined"
+                                color="secondary"
+                                onClick={handleLogout}
+                            >
+                                로그아웃
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </div>
                 </>
-            )}
-        </Container>
-    );
-};
+                )}
+            </Container>
+            );
+            };
 
-export default MainPage;
+            export default MainPage;
